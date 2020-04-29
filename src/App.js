43caf7e0a9ea89529d01d13bb5ff0e133259a64f
ggsuccess <ggsuccess@naver.com>
2020-04-29 12:47:22 +0900

    basic

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
+  const [value, setValue] = useState(0);
   return (
     <>
-      <h1>hahahahaha</h1>
+      <p>current count:{value}</p>
+      <button onClick={() => setValue(value + 1)}>click!up</button>
+      <button onClick={() => setValue(value - 1)}>click!down</button>
     </>
   );
 };

85759c4aa1e47efa3253d4f0e85c16d499dd2efa
ggsuccess <ggsuccess@naver.com>
2020-04-30 21:21:43 +0900

    set

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React, { useState } from 'react';
+import React from 'react';
+import Movie from './Movie';
 
+// const App = () => {
+//   const [value, setValue] = useState(0);
+//   return (
+//     <>
+//       <p>current count:{value}</p>
+//       <button onClick={() => setValue(value + 1)}>click!up</button>
+//       <button onClick={() => setValue(value - 1)}>click!down</button>
+//     </>
+//   );
+// };
+const movies = [
+  { title: 'Avengers:Endgame', poster: '' },
+  { title: 'Harry Potter and the Deathly Hallows', poster: '' },
+  { title: 'the Lord of the Rings: the Return of the King', poster: '' },
+  { title: 'Star Wars', poster: '' },
+];
 const App = () => {
-  const [value, setValue] = useState(0);
   return (
     <>
-      <p>current count:{value}</p>
-      <button onClick={() => setValue(value + 1)}>click!up</button>
-      <button onClick={() => setValue(value - 1)}>click!down</button>
+      {movies.map((movie) => {
+        <Movie title={movie.title} poster={movie.poster} />;
+      })}
     </>
   );
 };
---
+++ src/Movie.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Movie.css';
+
+const Movie = ({ title, poster }) => {
+  return (
+    <>
+      <table>
+        <tr>
+          <th></th>
+        </tr>
+      </table>
+    </>
+  );
+};
+
+export default Movie;

9fcfbca5fa7eb9bc44f48dea1e3af4893158d037
ggsuccess <ggsuccess@naver.com>
2020-05-04 13:27:33 +0900

    set

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Movie from './Movie';
-import Input from './Input';
+//import Input from './Input';
 
 // const App = () => {
 //   const [value, setValue] = useState(0);
@@ -14,47 +14,18 @@ import Input from './Input';
 // };
 
 const App = () => {
-  const movies = [
-    {
-      title: 'Avengers:Endgame',
-      poster:
-        'https://upload.wikimedia.org/wikipedia/en/0/0d/Avengers_Endgame_poster.jpg',
-      id: 0,
-    },
-    {
-      title: 'Harry Potter and the Deathly Hallows',
-      poster:
-        'https://upload.wikimedia.org/wikipedia/en/d/df/Harry_Potter_and_the_Deathly_Hallows_%E2%80%93_Part_2.jpg',
-      id: 1,
-    },
-    {
-      title: 'the Lord of the Rings: the Return of the King',
-      poster:
-        'https://img.discogs.com/Sms0zP1IMbStl_jmnnU0wNKNX3o=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-12458994-1535698027-9920.jpeg.jpg',
-      id: 2,
-    },
-    {
-      title: 'Star Wars the rise of skywalker',
-      poster:
-        'https://lumiere-a.akamaihd.net/v1/images/star-wars-the-rise-of-skywalker-theatrical-poster-1000_ebc74357.jpeg?region=0%2C0%2C891%2C1372',
-      id: 3,
-    },
-  ];
-  // movies.map((movie) => {
-  //   return (
-  //     <div>
-  //       <Movie key={movie.id} title={movie.title} poster={movie.poster} />
-  //     </div>
-  //   );
-  // });
-  return (
-    <>
-      <Input />
-      {movies.map((movie) => (
-        <Movie key={movie.id} title={movie.title} poster={movie.poster} />
-      ))}
-    </>
-  );
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    async function fetchMovies() {
+      const res = await fetch('');
+      res
+        .json()
+        .then((res) => setMovies(res))
+        .catch((err) => console.error(err));
+    }
+    fetchMovies();
+  });
+  return <></>;
 };
 
 export default App;
--- src/Input.js
+++ src/Input.js
@@ -1,16 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-const Input = () => {
-  const [title, setTitle] = useState('');
-
+const Input = ({ value }) => {
   const onChange = (e) => {
     console.log(e.target.value);
   };
   return (
     <>
       제목추가<input type="text" onChange={onChange}></input>
-      사진추가(사진주소)<input type="text"></input>
-      <button onClick={}>추가!</button>
+      사진추가(사진주소)<input type="text" onChange={onChange}></input>
+      <button >추가!</button>
     </>
   );
 };
